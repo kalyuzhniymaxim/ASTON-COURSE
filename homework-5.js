71b2dba8da07cda417e44f901fd84c4cc93c5f80
Maxim <kalyuzhniy-maxim2017@yandex.ru>
2024-04-06 15:43:01 +0300

    homework5

--- homework-5.js
+++ homework-5.js
@@ -1,38 +1,86 @@
 //1.
-// const a = {b: 1},
-//     c = Object.create(a);
-//
-// console.log(c.b); // ?
-// delete c.b;
-// console.log(c.b); // ?
-// delete a.b;
-// console.log(c.b); // ?
-// a.z = 2;
-// console.log(c.z); // ?
-// c.z = 3;
-// console.log(a.z); // ?
+const a = {b: 1},
+    c = Object.create(a);
+
+console.log(c.b); // 1
+delete c.b;
+console.log(c.b); // 1
+delete a.b;
+console.log(c.b); // undefuned
+a.z = 2;
+console.log(c.z); // 2
+c.z = 3;
+console.log(a.z); // 2
 
 // 2.
 
-// const promise = new Promise(() => {
-// })
-// promise.prototype === Promise.__proto__ // ?
-//
-// const obj = {}
-// obj.__proto__ === Object.prototype // ?
-//
-// new Array([]).__proto__ === Array.prototype // ?
-//
-// function Fn1 () {}
-// function Fn2 () {}
-// Fn1.constructor === Fn2.constructor // ?
-//
-// Fn1.prototype === Fn2.prototype // ?
+const promise = new Promise(() => {})
+promise.prototype === Promise.__proto__ // false
+
+const obj = {}
+obj.__proto__ === Object.prototype // true
+
+new Array([]).__proto__ === Array.prototype // true
+
+function Fn1 () {}
+function Fn2 () {}
+Fn1.constructor === Fn2.constructor // true 
+
+Fn1.prototype === Fn2.prototype // false
 //3.
 
 // У вас есть два конструктора, Animal и Bird.
 // Каждый объект типа Bird должен наследовать метод speak от Animal.
 // Однако, Bird также должен иметь свой собственный метод fly.
+class Animal{
+    constructor(name) {
+        this.name = name;
+        
+    }
+    speak(){
+        console.log('Some generic sound')
+    }
+}
+class Bird extends Animal{
+    constructor(name){
+        super(name)
+    }
+    
+    fly(){
+        console.log('Flying high!')
+    }
+}
+const animal = new Animal("Дженни");
+const bird = new Bird("Воробей");
+
+animal.speak(); // "Some generic sound"
+bird.speak();   // "Some generic sound"
+bird.fly();     // "Flying high!"
+
+// вариант 2
+ function Animal(name) {
+     this.name = name
+ }
+
+ Animal.prototype.speak = function() {
+     console.log('Some generic sound')
+ }
+
+ function Bird(name) {
+     Animal.call(this, name)
+ }
+
+ Bird.prototype.fly = function() {
+     console.log('Flying high!')
+ }
+
+ Object.setPrototypeOf(Bird.prototype, Animal.prototype)
+ const animal1 = new Animal("Дженни");
+const bird1 = new Bird("Воробей");
+
+animal1.speak(); // "Some generic sound"
+bird1.speak();   // "Some generic sound"
+bird1.fly();     // "Flying high!"
 
 // Создайте конструктор Animal, который принимает параметр name и устанавливает его как свойство объекта.
 // Добавьте метод speak в Animal, который выводит в консоль звук, издаваемый животным (например, "Some generic sound").
